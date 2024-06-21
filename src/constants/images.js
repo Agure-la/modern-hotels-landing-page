93749f7b21ce3a4871392236632ac316c7b79cfc
Agure <lameckagure@gmail.com>
2024-06-21 14:14:27 +0300

    completed navbar and haeders

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import g1 from '../assets/g1.webp';
+import bg from '../assets/bg.jpeg'
+import welcome from '../assets/welcome.jpg'
+import spoon from '../assets/spoon.jpg'
+
+export default {
+g1,
+bg,
+welcome,
+spoon,
+};
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='app__header app__wrapper section__padding' id="home">
+      <div className='app__wrapper_info'>
+        <SubHeading title='Get new Taste' />
+        <h1 className='app_header-h1'>The Key to fine Dishes</h1>
+        <p className='p__opensans' style={{margin: '2rem 0'}}>Enjoy delicious flavors and warm hospitality. Experience gourmet dishes that delight the senses. Indulge in culinary excellence, crafted just for you.</p>
+        <button type='button' className='custom__button'>Explore Menu</button>
+      </div>
+
+      <div className='app__wrapper_img'>
+        <img src={images.welcome} alt='header_img' />
+      </div>
     </div>
   )
 }
